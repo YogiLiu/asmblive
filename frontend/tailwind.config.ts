a13c6d55499b9e446fcbe7d54a74311eae0bd5b4
YogiLiu <YogiLiu@outlook.com>
2024-07-26 17:18:55 +0800

    feat(frontend): add iconify and phosphor

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'tailwindcss'
 import daisyui from 'daisyui'
+import { addIconSelectors } from '@iconify/tailwind'
 
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [addIconSelectors(['ph']), daisyui],
 } satisfies Config
