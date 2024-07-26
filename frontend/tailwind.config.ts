5450c0b78118eca88dfd5a27b6223217b1b4aca8
YogiLiu <YogiLiu@outlook.com>
2024-07-26 11:32:23 +0800

    feat(frontend): add daisyui

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from 'tailwindcss'
+import daisyui from 'daisyui'
 
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 } satisfies Config
